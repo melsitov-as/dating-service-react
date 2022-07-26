bf64e7ee96340a187ab269c40d096a4c92c02aa0
Александр <mas2005@inbox.ru>
2022-07-26 19:51:58 +0300

    Добавляет компонент пагинации

---
+++ src/app/components/users.jsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import Pagination from './pagination';
+import User from './user';
+const Users = ({ users, ...rest }) => {
+	const count = users.length;
+	const pageSize = 4;
+
+	const handlePageChange = () => {
+		console.log('pageChange');
+	};
+	return (
+		<>
+			{count > 0 && (
+				<table className='table'>
+					<thead>
+						<tr>
+							<th scope='col'>Имя</th>
+							<th scope='col'>Качества</th>
+							<th scope='col'>Профессия</th>
+							<th scope='col'>Встретился, раз</th>
+							<th scope='col'>Оценка</th>
+							<th scope='col'>Избранное</th>
+							<th />
+						</tr>
+					</thead>
+					<tbody>
+						{users.map((user) => (
+							<User key={user._id} {...rest} {...user} />
+						))}
+					</tbody>
+				</table>
+			)}
+			<Pagination
+				itemsCount={count}
+				pageSize={pageSize}
+				onPageChange={handlePageChange}
+			/>
+		</>
+	);
+};
+
+export default Users;
