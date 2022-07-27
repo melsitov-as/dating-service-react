eb7725e390ada05ce9fd56f9076b22ab23eaf911
Александр <mas2005@inbox.ru>
2022-07-27 18:26:37 +0300

    Добавляет фильтрацию пагинацию и очистить

--- src/app/components/groupList.jsx
+++ src/app/components/groupList.jsx
@@ -1,15 +1,60 @@
-import React from 'react';
+import React from "react";
+import PropTypes from "prop-types";
 
-const GroupList = (props) => {
-	return (
-		<ul class='list-group'>
-			<li className='list-group-item'>An item</li>
-			<li className='list-group-item'>A second item</li>
-			<li className='list-group-item'>A third item</li>
-			<li className='list-group-item'>A fourth item</li>
-			<li className='list-group-item'>And a fifth one</li>
-		</ul>
-	);
+const GroupList = ({
+    items,
+    valueProperty,
+    contentProperty,
+    onItemSelect,
+    selectedItem
+}) => {
+    if (!Array.isArray(items)) {
+        return (
+            <ul className="list-group">
+                {Object.keys(items).map((item) => (
+                    <li
+                        key={items[item][valueProperty]}
+                        className={
+                            "list-group-item" +
+                            (items[item] === selectedItem ? " active" : "")
+                        }
+                        onClick={() => onItemSelect(items[item])}
+                        role="button"
+                    >
+                        {items[item][contentProperty]}
+                    </li>
+                ))}
+            </ul>
+        );
+    }
+    return (
+        <ul className="list-group">
+            {items.map((item) => (
+                <li
+                    key={item[valueProperty]}
+                    className={
+                        "list-group-item" +
+                        (item === selectedItem ? " active" : "")
+                    }
+                    onClick={() => onItemSelect(item)}
+                    role="button"
+                >
+                    {item[contentProperty]}
+                </li>
+            ))}
+        </ul>
+    );
+};
+GroupList.defaultProps = {
+    valueProperty: "_id",
+    contentProperty: "name"
+};
+GroupList.propTypes = {
+    items: PropTypes.oneOfType([PropTypes.object, PropTypes.array]),
+    valueProperty: PropTypes.string.isRequired,
+    contentProperty: PropTypes.string.isRequired,
+    onItemSelect: PropTypes.func,
+    selectedItem: PropTypes.object
 };
 
 export default GroupList;
