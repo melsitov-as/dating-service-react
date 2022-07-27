f5e96f5766c32b338703aacf18dffdaddf2f57c4
Александр <mas2005@inbox.ru>
2022-07-27 19:24:04 +0300

    Реализует основную логику сортировки

--- src/app/components/users.jsx
+++ src/app/components/users.jsx
@@ -1,99 +1,92 @@
-import React, { useState, useEffect } from "react";
-import PropTypes from "prop-types";
-import { paginate } from "../utils/paginate";
-import Pagination from "./pagination";
-import User from "./user";
-import api from "../api";
-import GroupList from "./groupList";
-import SearchStatus from "./searchStatus";
+import React, { useState, useEffect } from 'react';
+import PropTypes from 'prop-types';
+import { paginate } from '../utils/paginate';
+import Pagination from './pagination';
+import api from '../api';
+import GroupList from './groupList';
+import SearchStatus from './searchStatus';
+import UsersTable from './usersTable';
+import _ from 'lodash';
 const Users = ({ users: allUsers, ...rest }) => {
-    const [currentPage, setCurrentPage] = useState(1);
-    const [professions, setProfession] = useState();
-    const [selectedProf, setSelectedProf] = useState();
+	const [currentPage, setCurrentPage] = useState(1);
+	const [professions, setProfession] = useState();
+	const [selectedProf, setSelectedProf] = useState();
+	const [sortBy, setSortBy] = useState({ iter: 'name', order: 'asc' });
 
-    const pageSize = 2;
-    useEffect(() => {
-        api.professions.fetchAll().then((data) => setProfession(data));
-    }, []);
-    useEffect(() => {
-        setCurrentPage(1);
-    }, [selectedProf]);
+	const pageSize = 2;
+	useEffect(() => {
+		api.professions.fetchAll().then((data) => setProfession(data));
+	}, []);
+	useEffect(() => {
+		setCurrentPage(1);
+	}, [selectedProf]);
 
-    const handleProfessionSelect = (item) => {
-        setSelectedProf(item);
-    };
+	const handleProfessionSelect = (item) => {
+		setSelectedProf(item);
+	};
 
-    const handlePageChange = (pageIndex) => {
-        setCurrentPage(pageIndex);
-    };
-    const filteredUsers = selectedProf
-        ? allUsers.filter(
-              (user) =>
-                  JSON.stringify(user.profession) ===
-                  JSON.stringify(selectedProf)
-          )
-        : allUsers;
+	const handlePageChange = (pageIndex) => {
+		setCurrentPage(pageIndex);
+	};
+	const handleSort = (item) => {
+		if (sortBy.iter === item) {
+			setSortBy((prevState) => ({
+				...prevState,
+				order: prevState.order === 'asc' ? 'desc' : 'acs',
+			}));
+		} else {
+			setSortBy({ iter: item, order: 'asc' });
+		}
+	};
+	const filteredUsers = selectedProf
+		? allUsers.filter(
+				(user) =>
+					JSON.stringify(user.profession) === JSON.stringify(selectedProf)
+		  )
+		: allUsers;
 
-    const count = filteredUsers.length;
-    const usersCrop = paginate(filteredUsers, currentPage, pageSize);
-    const clearFilter = () => {
-        setSelectedProf();
-    };
+	const count = filteredUsers.length;
 
-    return (
-        <div className="d-flex">
-            {professions && (
-                <div className="d-flex flex-column flex-shrink-0 p-3">
-                    <GroupList
-                        selectedItem={selectedProf}
-                        items={professions}
-                        onItemSelect={handleProfessionSelect}
-                    />
-                    <button
-                        className="btn btn-secondary mt-2"
-                        onClick={clearFilter}
-                    >
-                        {" "}
-                        Очистить
-                    </button>
-                </div>
-            )}
-            <div className="d-flex flex-column">
-                <SearchStatus length={count} />
-                {count > 0 && (
-                    <table className="table">
-                        <thead>
-                            <tr>
-                                <th scope="col">Имя</th>
-                                <th scope="col">Качества</th>
-                                <th scope="col">Профессия</th>
-                                <th scope="col">Встретился, раз</th>
-                                <th scope="col">Оценка</th>
-                                <th scope="col">Избранное</th>
-                                <th />
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {usersCrop.map((user) => (
-                                <User {...rest} {...user} key={user._id} />
-                            ))}
-                        </tbody>
-                    </table>
-                )}
-                <div className="d-flex justify-content-center">
-                    <Pagination
-                        itemsCount={count}
-                        pageSize={pageSize}
-                        currentPage={currentPage}
-                        onPageChange={handlePageChange}
-                    />
-                </div>
-            </div>
-        </div>
-    );
+	const sortedUsers = _.orderBy(filteredUsers, [sortBy.iter], [sortBy.order]);
+	const usersCrop = paginate(sortedUsers, currentPage, pageSize);
+	const clearFilter = () => {
+		setSelectedProf();
+	};
+
+	return (
+		<div className='d-flex'>
+			{professions && (
+				<div className='d-flex flex-column flex-shrink-0 p-3'>
+					<GroupList
+						selectedItem={selectedProf}
+						items={professions}
+						onItemSelect={handleProfessionSelect}
+					/>
+					<button className='btn btn-secondary mt-2' onClick={clearFilter}>
+						{' '}
+						Очистить
+					</button>
+				</div>
+			)}
+			<div className='d-flex flex-column'>
+				<SearchStatus length={count} />
+				{count > 0 && (
+					<UsersTable users={usersCrop} {...rest} onSort={handleSort} />
+				)}
+				<div className='d-flex justify-content-center'>
+					<Pagination
+						itemsCount={count}
+						pageSize={pageSize}
+						currentPage={currentPage}
+						onPageChange={handlePageChange}
+					/>
+				</div>
+			</div>
+		</div>
+	);
 };
 Users.propTypes = {
-    users: PropTypes.array
+	users: PropTypes.array,
 };
 
 export default Users;
