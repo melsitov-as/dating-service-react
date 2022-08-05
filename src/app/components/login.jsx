9636cebf0ec924450a42260a6f79f9ce5c702ef2
Александр <mas2005@inbox.ru>
2022-08-05 11:16:10 +0300

    Добавляет BrowserRouter к приложению

--- src/app/components/login.jsx
+++ src/app/components/login.jsx
@@ -1,12 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Login = () => {
+	const [email, setEmail] = useState('');
+	const handleChange = (e) => {
+		setEmail(e.target.value);
+		console.log(e.target.value);
+	};
 	return (
 		<>
 			<form action=''>
 				<div>
 					<label htmlFor='email'>Email</label>
-					<input type='text' id='email'></input>
+					<input
+						type='text'
+						id='email'
+						value={email}
+						onChange={handleChange}></input>
 				</div>
 				<div>
 					<label htmlFor='email'>Пароль</label>
--- src/index.js
+++ src/index.js
@@ -3,8 +3,13 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import 'bootstrap/dist/css/bootstrap.css';
 import App from './app/App';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import { createRoot } from 'react-dom/client';
 const container = document.getElementById('root');
 const root = createRoot(container); // createRoot(container!) if you use TypeScript
-root.render(<App />);
+root.render(
+	<BrowserRouter>
+		<App />
+	</BrowserRouter>
+);
